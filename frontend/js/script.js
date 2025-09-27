4e9c263395f050a5b4d7e5eb89535a0b4bfc202a
Bosco <90090974+Bosco1262@users.noreply.github.com>
2025-09-27 16:26:54 +0000

    优化标题栏样式，更新主题切换功能

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,18 +1,6 @@
 const body = document.body;
 const theme_switch_btn = document.getElementById("theme-switch-btn");
-const announcement_icon = document.getElementById("announcement-icon");
-const user_icon = document.getElementById("user-icon");
 
 theme_switch_btn.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
-
-  if (body.classList.contains("dark-mode")) {
-    announcement_icon.src =
-      "./images/titlebar/announcement/announcement-darkmode.png";
-    user_icon.src = "./images/titlebar/user-center/user-darkmode.png";
-  } else {
-    announcement_icon.src =
-      "./images/titlebar/announcement/announcement-whitemode.png";
-    user_icon.src = "./images/titlebar/user-center/user-whitemode.png";
-  }
 });
